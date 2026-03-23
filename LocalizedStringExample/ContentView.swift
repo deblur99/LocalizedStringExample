6e9c59bb738730b77a92dff58a306c0f160d3512
deblur99 <deblur99@kakao.com>
2026-03-23 15:45:06 +0900

    Initial Commit

---
+++ LocalizedStringExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizedStringExample
+//
+//  Created by 한현민 on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalizedStringExample/LocalizedStringExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedStringExampleApp.swift
+//  LocalizedStringExample
+//
+//  Created by 한현민 on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedStringExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
